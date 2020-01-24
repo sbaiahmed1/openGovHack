f970a5ea35d7f4a4efaab71819f670e7f3aaf951
Ahmed <ahmedclubust@gmail.com>
2020-01-24 13:15:36 +0100

    Init

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {AppBar,Toolbar,Typography,Button,IconButton} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import useStyles from './navbarStyle'
+
+export default function ButtonAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
