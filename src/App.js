1e35acf5a57b68c90eb91a605a30f78a5a45b4b6
Ahmed <ahmedclubust@gmail.com>
2020-02-02 17:20:47 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Link, Router, Route, browserHistory} from 'react-router'
+import navbar from "./components/navbar/navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Router history={browserHistory}>
+              <Route exact path="/" component={navbar} />
+              {/*<Route path="/add-course" component={AddCourse} />*/}
+              {/*<Route path="/display-item" component={ButtonAppBar} />*/}
+              {/*<Route path="/edit" component={FileUpload} />*/}
+              {/*<Route path={'/login'} component={Login}/>*/}
+              {/*<Route path={'/signup'} component={Register}/>*/}
+              {/*<Route path={'/all-courses'} component={ViewAllCourses}/>*/}
+          </Router>
+      </div>
+
   );
 }
 
---
+++ src/views/signin/addSigninStyle.js
@@ -0,0 +1,19 @@
+const addloginStyle = {
+    formContainer:{
+        padding: '5%',
+        alignSelf:'Center'
+    },
+    signin:{
+        marginLeft: '45%',
+        marginTop:'5%',
+        alignSelf:'Center',
+        color:'white',
+        fontSize:30
+    },
+    inputContainer:{
+        paddingLeft :'1%',
+        paddingRight:'1%'
+    }
+};
+export default addloginStyle;
+
